f75608b3b8cb92fb8f2ec16cc347965c3762e8ac
KilluaZodlyck55 <106536320+KilluaZodlyck55@users.noreply.github.com>
2022-08-28 10:42:17 +0700

    Create enable.js

---
+++ enable.js
@@ -0,0 +1,15 @@
+anticall: false, // Auto Reject better than autoblock
+
+autoread: false, // If true all chats are automatically read
+
+nyimak: false, // No bot, just print received messages and add users to database
+
+restrict: false, // Enables restricted plugins (which can lead your number to be banned if used too often)
+
+self: false, // Activate self mode (Ignores other)
+
+pconly: false, // If that chat not from private bot, bot will ignore
+
+gconly: false, // If that chat not from group, bot will ignore
+
+jadibot: false, 
